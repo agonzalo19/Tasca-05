a0e8c83654252ebab6413da3113940a62538de26
agonzalo19 <74074576+agonzalo19@users.noreply.github.com>
2021-04-19 00:26:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+// --------  Exercicio 1
+//console.log("Hola Mundo")
+
+// -------- Exercicio 2
+//alert("Mi nombre es Andrea")
+
+// --------  Exercicio 3
+//var nombre = "Andrea "
+//var apellido = "Gonzalo"
+//var mensaje = nombre + apellido;
+//console.log(mensaje)
+
+// --------  Exercicio 4
+//var numeroa = 34
+//var numerob = 56
+//var resultado = numeroa + numerob;
+//var texto = "La suma entre " + numeroa + " i " + numerob  + " es " + resultado;
+//console.log(texto);
+
+// --------  Exercicio 5
+//var nota_examen = 5.5;
+//var resultado = nota_examen > 5;
+//if(resultado == true){
+//console.log("Felicidades, has aprobado con un "+nota_examen)
+//} else {
+//console.log("Oh, has suspendido con un "+nota_examen)
+//}
+	
+
+// -------- Exercicio 6
+// var ColorCotxe = "verd";
+// switch(ColorCotxe) {
+// 	case "blau" :
+// 		console.log("Tinc un cotxe de color blau.")
+// 		break;
+// 	
+// 	case "verd" :
+// 		console.log("Tinc un cotxe de color verd.")
+// 		break;
+// }
+// 
+// var Lletra = "u";
+// vswitch(Lletra) {
+// 	case "o" :
+// 		console.log("Tinc un cotxe"+ColorCotxe)
+// 		break;
+	
+// 	case "u" :
+// 		console.log("Tinc un cutxe de culur "+ColorCotxe+".")
+// 		break;
+// }
+
+// -------- Exercicio 7
+//var objectes = ['taula', 'cadira', 'ordinador', 'libreta'];
+
+//for(var i = 0; i<4 ;i++){
+//	console.log("L'objecte " + objectes[i] + " està a la posició " + i)
+//}
+
+// --------  Exercicio 8
+
+//var operador = "suma";
+//var operador = "multiplicar";
+//var operador = "resta";
+//var a = 20;
+//var b = 40;
+	
+//	function suma () {
+//		return a + b;
+//	}
+
+//	function resta () {
+//		return a - b;
+//	}
+
+//	function multiplicar () {
+//		return a * b;
+//	}
+
+//var calculadora = multiplicar (40,20);
+//console.log(calculadora);
